ee316f73d27e8694d60f26b8e330f956e1d7c4d5
halitakkus <halitakkus03@gmail.com>
2020-05-04 11:33:41 +0300

    Repository pattern uygulandı ve Entity Framework ile Implementasyonu yapıldı.

---
+++ SmartLens.Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,17 @@
+﻿using SmartLens.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace SmartLens.Core.DataAccess
+{
+   public interface IEntityRepository<T> where T : class , IEntity, new()
+    {
+        T  Get(Expression<Func<T,bool>> filter = null);
+        List<T> GetList(Expression<Func<T, bool>> filter = null);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
