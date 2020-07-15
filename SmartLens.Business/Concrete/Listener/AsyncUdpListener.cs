9a2e770ab5c9870a479c1c1614190469fed0245a
halitakkus <halitakkus03@gmail.com>
2020-07-15 19:49:07 +0300

    Udp server statikleştirildi, singletion tasarım desenine uygun hale getirildi.

---
+++ SmartLens.Business/Concrete/Listener/AsyncUdpListener.cs
@@ -0,0 +1,60 @@
+﻿using SmartLens.Business.Abstract.Listener;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartLens.Business.Concrete.Listener
+{
+    public class AsyncUdpListener : IListener,IDisposable
+    {
+        private static UdpClient _listener { get; set; }
+        private static IPEndPoint _groupEP { get; set; }
+        private static AsyncUdpListener _asyncUdpServer;
+     
+        public static AsyncUdpListener currentUdpServer(UdpClient udpClient, IPEndPoint ıPEndPoint)
+        {
+            if (_asyncUdpServer ==null)
+            {
+                _asyncUdpServer = new AsyncUdpListener();
+            }
+            _listener = udpClient;
+            _groupEP = ıPEndPoint;
+            return _asyncUdpServer;
+        }
+        public static void newUdpClient(UdpClient udpClient)
+        {
+            _listener = udpClient;
+        }
+        public async Task<byte[]> StartListener()
+        {
+            try
+            {
+                var receiveResult = await _listener.ReceiveAsync();
+                _listener.Close();
+                return receiveResult.Buffer;
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                _listener.Close();
+            }
+            return new byte[] { };
+        }
+
+        public string Message()
+        {
+            return "Waiting for broadcast/ UDP";
+        }
+
+        public void Dispose()
+        {
+            _listener = null;
+        }
+    }
+}
