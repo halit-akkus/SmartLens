ba64de200c82a7c2ae6a5e09c9fc62a6fbfc5a6c
halitakkus <halitakkus03@gmail.com>
2020-07-15 19:50:27 +0300

    Asenkron metotlar bir arada toplandı.

---
+++ SmartLens.Transmission/ImageConvert.cs
@@ -0,0 +1,37 @@
+﻿using SmartLens.Business.Abstract.Listener;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartLens.Transmission
+{
+   public static class ImageConvert
+    {
+        public static Image byteToImage(byte[] byteArrayIn)
+        {
+            using (var fileStream = new MemoryStream(byteArrayIn))
+            {
+                return Image.FromStream(fileStream);
+            }
+        }
+        private static Image ImageToGray(Bitmap bmp)
+        {
+            for (int y = 0; y < bmp.Height; y++)
+            {
+                for (int x = 0; x < bmp.Width; x++)
+                {
+                    Color old = bmp.GetPixel(x, y);
+                    int avg = (old.R + old.G + old.B) / 3;
+                    Color yeni = Color.FromArgb(old.A, avg, avg, avg);
+                    bmp.SetPixel(x, y, yeni);
+                }
+            }
+            return bmp;
+        }
+
+       
+    }
+}
