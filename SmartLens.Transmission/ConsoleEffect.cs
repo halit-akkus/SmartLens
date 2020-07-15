79940043fd020e8e861fbbc3d7393c03f9167ff2
halitakkus <halitakkus03@gmail.com>
2020-07-15 19:50:56 +0300

    Konsol efektleri değiştirildi.

---
+++ SmartLens.Transmission/ConsoleEffect.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace SmartLens.Transmission
+{
+   public class ConsoleEffect
+    {
+        private static ConsoleColor[] _colors = { ConsoleColor.DarkYellow, ConsoleColor.Gray, ConsoleColor.DarkGreen, ConsoleColor.DarkRed, ConsoleColor.DarkCyan };
+        private static Random _rnd = new Random();
+        public ConsoleEffect()
+        {
+            Console.WindowHeight = (Console.LargestWindowHeight * 9 / 10);
+        }
+        public static void SetColor(ConsoleColor fore)
+        {
+            Console.ForegroundColor = fore;
+        }
+        public static void SetColor()
+        {
+            Console.ForegroundColor = _colors[_rnd.Next(0, _colors.Length)];
+        }
+        public static void ClearCurrent(int size)
+        {
+            int currentLineCursor = Console.CursorTop;
+            Console.SetCursorPosition(size, Console.CursorTop);
+            Console.Write(new string(' ', Console.WindowWidth));
+            Console.SetCursorPosition(size, currentLineCursor);
+        }
+        public static void Effect(object length)
+        {
+            string[] result = { "", ".", "..", "...", "", ".", " .", "  ." };
+            Thread.Sleep(2000);
+            while (true)
+            {
+                for (int i = 0; i < result.Length; i++)
+                {
+                    Console.Write(result[i]);
+                    Thread.Sleep(450);
+                    ClearCurrent((int)length);
+                }
+            }
+        }
+    }
+}
