61b1604ff5e1e870274eab79712e37cbcbbd58b5
halitakkus <halitakkus03@gmail.com>
2020-07-17 16:14:36 +0300

    enum yapısı eklendi.

---
+++ SmartLens.Transmission/Concrate/Standart.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartLens.Transmission.Concrate
+{
+   public static class Standart
+    {
+       public enum  ListenerType {Tcp,Udp,TcpAndUdp}
+    }
+}
