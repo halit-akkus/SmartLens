efc6fdad7c85baa7556c7761ee7158ecfe4e06cf
halitakkus <halitakkus03@gmail.com>
2020-10-04 02:41:10 -0700

    Settings yapılandırıldı.

---
+++ src/SmartLens.Transmission/Tdo/ISettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartLens.Transmission.Tdo
+{
+    public interface ISettings
+    {
+        public int frontServerPort { get; set; }
+        public int serviceServerPort { get; set; }
+        public string defaultProtocol { get; set; }
+        public  Task<string> getJson();
+    }
+}
---
+++ src/SmartLens.Transmission/Tdo/Settings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartLens.Transmission.Tdo
+{
+    public class Settings : ISettings
+    {
+
+        public int frontServerPort { get; set; }
+        public int serviceServerPort { get; set; }
+        public string defaultProtocol { get; set; }
+        public async Task<string> getJson()
+        {
+            var filePath = "Settings.json";
+
+            using (StreamReader sr = new StreamReader(filePath, Encoding.UTF7))
+            {
+                string readText = await sr.ReadToEndAsync();
+                return readText;
+            }
+        }
+    }
+}
